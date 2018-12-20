b1cf4b98f1c45a663486a74dfdc6726954212812
Eric Chase <eric.nalley+github@gmail.com>
2018-12-19 23:27:40 -0600

    cleaned up code, added comments, project is finished

--- code/src/main.js
+++ code/src/main.js
@@ -3,13 +3,8 @@
 let bitVector = function (size) {
   this.store = new Uint32Array(size);
 };
-bitVector.prototype.setBit = function (bit, set) {
-  const index = bit >> 5;
-  if (set) {
-    this.store[index] = this.store[index] | (1 << (bit & 31));
-  } else {
-    this.store[index] = this.store[index] & ~(1 << (bit & 31));
-  }
+bitVector.prototype.setBit = function (bit) {
+  this.store[bit >> 5] |= 1 << (bit & 31);
 };
 bitVector.prototype.isSet = function (bit) {
   return (this.store[bit >> 5] & (1 << (bit & 31))) !== 0;
@@ -19,18 +14,22 @@ let maxCheck = 472882240; // make sufficiently high
 let sieve = new bitVector((maxCheck >> 5) + 1);
 
 function prep() {
+  // find all multiples less than maxCheck at the start
+  // no need to check even numbers
   for (let i = 3; i < 32000; i += 2) {
     if (sieve.isSet(i)) continue;
-    if (i > maxCheck)
-      throw new Error("'maxCheck' too small");
     for (let x = i + i; x <= maxCheck; x += i)
       sieve.setBit(x, true);
   }
 }
 
+// A simple Sieve of Eratosthenes with some general coding
+// optimizations. Note that this code generates prime numbers
+// under a maximal cutoff defined by maxCheck.
 class Primes {
   static* stream() {
     yield 2;
+    // no need to check even numbers
     for (let i = 3; i <= maxCheck; i += 2)
       if (!sieve.isSet(i))
         yield i;
