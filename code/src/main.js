b893b28b71035ae81ed67e47f69d26e3e970f6d4
Eric Chase <eric.nalley+github@gmail.com>
2018-12-19 23:05:02 -0600

    improved the sieve algorithm

--- code/src/main.js
+++ code/src/main.js
@@ -4,33 +4,40 @@ let bitVector = function (size) {
   this.store = new Uint32Array(size);
 };
 bitVector.prototype.setBit = function (bit, set) {
+  const index = bit >> 5;
   if (set) {
-    this.store[(bit >> 5)] = this.store[(bit >> 5)] | (1 << (bit & 31));
+    this.store[index] = this.store[index] | (1 << (bit & 31));
   } else {
-    this.store[(bit >> 5)] = this.store[(bit >> 5)] & ~(1 << (bit & 31));
+    this.store[index] = this.store[index] & ~(1 << (bit & 31));
   }
 };
 bitVector.prototype.isSet = function (bit) {
-  return (this.store[(bit >> 5)] & (1 << (bit & 31))) !== 0;
+  return (this.store[bit >> 5] & (1 << (bit & 31))) !== 0;
 };
 
-let maxCheck = 472882050; // make sufficiently high
+let maxCheck = 472882240; // make sufficiently high
 let sieve = new bitVector((maxCheck >> 5) + 1);
-let prime = new bitVector((maxCheck >> 5) + 1);
 
-module.exports = class Primes {
-  static* stream() {
-    for (let i = 2; ; ++i) {
-      if (!prime.isSet(i)) {
-        if (sieve.isSet(i)) continue;
-        if (i > maxCheck)
-          throw new Error("'maxCheck' too small");
-        for (let x = i + i; x < maxCheck; x += i)
-          if (!sieve.isSet(x))
-            sieve.setBit(x, true);
-        prime.setBit(i, true);
-      }
-      yield i;
-    }
+function prep() {
+  for (let i = 3; i < 32000; i += 2) {
+    if (sieve.isSet(i)) continue;
+    if (i > maxCheck)
+      throw new Error("'maxCheck' too small");
+    for (let x = i + i; x <= maxCheck; x += i)
+      sieve.setBit(x, true);
   }
+}
+
+class Primes {
+  static* stream() {
+    yield 2;
+    for (let i = 3; i <= maxCheck; i += 2)
+      if (!sieve.isSet(i))
+        yield i;
+  }
+}
+
+module.exports = {
+  prep: prep,
+  Primes: Primes
 };
--- code/test/main.test.js
+++ code/test/main.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const Primes = require('../src/main.js');
+const {prep, Primes} = require('../src/main.js');
 
 const verify = (n, ...a) => function () {
   const stream = Primes.stream();
@@ -8,12 +8,16 @@ const verify = (n, ...a) => function () {
     expect(stream.next().value)
       .to.equal(v);
 };
-describe('Small numbers.', () => {
+
+describe('1: Prep primes.', () => {
+  it('should run faster than 5 seconds', () => prep());
+});
+describe('2: Small numbers.', () => {
   it('0 - 10', verify(0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29));
   it('10 - 20', verify(10, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71));
   it('100 - 110', verify(100, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601));
   it('1000 - 1010', verify(1000, 7927, 7933, 7937, 7949, 7951, 7963, 7993, 8009, 8011, 8017));
 });
-describe('Large numbers.', () => {
-  it('25e6 - 25e6', verify(25e6));
+describe('3: Large numbers.', () => {
+  it('0 - 10', verify(25e6, 472882049, 472882073, 472882099, 472882103, 472882117, 472882141, 472882169, 472882171, 472882199, 472882219));
 });
